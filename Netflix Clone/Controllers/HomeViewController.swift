e56e5f373dc3c886bbd5ab20eea1f0400f383431
Rabia Yolcu <rabia.yolcu.1903@gmail.com>
2024-08-20 16:00:47 +0300

    add file: APICaller and Movie

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -31,6 +31,8 @@ class HomeViewController: UIViewController{
         let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
         homeFeedTable.tableHeaderView = headerView
         
+        getTrendingMovies()
+        
     }
     
     private func configureNavbar(){
@@ -51,6 +53,19 @@ class HomeViewController: UIViewController{
         homeFeedTable.frame = view.bounds
     }
 
+    private func getTrendingMovies(){
+        APICaller.shared.getTrendingMovies { results in
+            switch results {
+            
+            case .success(let movies):
+                print(movies)
+            case .failure(let error):
+                print(error)
+            
+            }
+        }
+    }
+    
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource{
